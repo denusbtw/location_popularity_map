b2a988a845736d5433762ac6d7f2236998ac3475
denusbtw <denisrozum@gmail.com>
2025-06-25 12:05:53 +0300

    feat(locations): create Category and Location models

--- config/settings/base.py
+++ config/settings/base.py
@@ -33,6 +33,7 @@ THIRD_PARTY_APPS = []
 LOCAL_APPS = [
     "test_task.core",
     "test_task.users",
+    "test_task.locations",
 ]
 
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
---
+++ test_task/locations/models.py
@@ -0,0 +1,49 @@
+from django.core.validators import (
+    MaxLengthValidator,
+    MinValueValidator,
+    MaxValueValidator,
+)
+from django.db import models
+
+from test_task.core.models import UUIDModel, TimestampedModel
+
+
+class Category(UUIDModel, TimestampedModel):
+    name = models.CharField(max_length=100, unique=True)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
+
+
+class Location(UUIDModel, TimestampedModel):
+    name = models.CharField(max_length=255)
+    description = models.TextField(validators=[MaxLengthValidator(5000)])
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.PROTECT,
+        related_name="locations",
+    )
+
+    latitude = models.DecimalField(
+        max_digits=9,
+        decimal_places=6,
+        validators=[MinValueValidator(-90), MaxValueValidator(90)],
+    )
+    longitude = models.DecimalField(
+        max_digits=9,
+        decimal_places=6,
+        validators=[MinValueValidator(-180), MaxValueValidator(180)],
+    )
+    address = models.CharField(max_length=255, unique=True)
+
+    is_active = models.BooleanField(default=True)
+    view_count = models.PositiveBigIntegerField(default=0)
+
+    class Meta:
+        ordering = ["-created_at"]
+
+    def __str__(self):
+        return self.name
